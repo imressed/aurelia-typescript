3f06b7193ae69613024defc23ee21b5999e3bc40
imressed <imressed@gmail.com>
2016-04-13 13:53:21 +0200

    feature observing name changes
    an example how to observe variable changes using aurelia binding engine

--- src/home.ts
+++ src/home.ts
@@ -1,9 +1,19 @@
+import {BindingEngine, inject} from 'aurelia-framework';
+
+@inject(BindingEngine)
 export class Home{
 
   name: string;
 
-  constructor(){
-    this.name = 'Vlad'
+  constructor(bindingEngine: BindingEngine){
+    this.name = 'Vlad';
 
+    let subscription = bindingEngine
+      .propertyObserver(this, 'name')
+      .subscribe(this.nameChange);
+  }
+
+  nameChange(newValue, oldValue){
+    console.log(`observeMe value changed from: ${oldValue} to:${newValue}`);
   }
 }
